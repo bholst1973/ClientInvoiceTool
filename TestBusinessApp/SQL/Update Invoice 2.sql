5fa44919268c2ab5a53461064c0013887c8a7f56
Unknown <holst.william@mayo.edu>
2018-07-16 06:58:19 -0500

    Update routine sql statement file.
    Update routine sql statement file.

--- TestBusinessApp/SQL/Update Invoice 2.sql
+++ TestBusinessApp/SQL/Update Invoice 2.sql
@@ -8,8 +8,8 @@ update Invoice set inv_Num = 14 where inv_Num = 15
 update Invoice set INV_Date = '2009-08-01' where INV_NUM = 22
 Update invoice set INV_Tax = 3.25 where INV_ID = 32
 
-Update invoice set INV_Cost = 0.00 where INV_Cost is null
-update invoice set INV_Paid = 1
+Update invoice set INV_TaxPaid = 0.00 where INV_TaxPaid is null
+update invoice set INV_Paid = 1 where INV_NUM < 4
 
 select * from Invoice where INV_TaxPaid is null
 
